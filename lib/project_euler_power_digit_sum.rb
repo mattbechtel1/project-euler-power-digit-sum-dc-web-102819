bdc9198e32b87ced91944376725cf904efcaa191
mattbechtel1 <bechma02@gmail.com>
2020-02-21 10:22:59 -0500

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,10 @@
 # code your solution here
+
+def power_digit_sum(base, exponent)
+    step_1 = base ** exponent
+    num_array = step_1.to_s.split(//)
+
+    num_array.reduce(0) {|memo, char| memo + char.to_i}
+end
+
+puts power_digit_sum(2, 4)
